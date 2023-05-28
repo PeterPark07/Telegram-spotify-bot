9362585208c9cb48ae9c7d449bdc3080b28b542f
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-28 14:49:30 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, request
 import time
+from flask import Flask, request
 import telebot
 from helper.log import send_log
 
@@ -12,104 +12,61 @@ last_message_id = None
 
 @app.route('/', methods=['POST'])
 def telegram():
-    # Process incoming updates
+    # Process incoming updates from Telegram
     if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
+        bot.process_new_updates([telebot.types.Update.de_json(request.get_data().decode('utf-8'))])
         return 'OK', 200
-
-# Handler for the '/start' command
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    send_log(bot, message)
-    bot.reply_to(message, 'Welcome to the Spotify Downloader Bot!\n\nSend me a song name or a Spotify link to download.')
-
-@bot.message_handler(commands=['on'])
-def handle_on(message):
+    
+@bot.message_handler(commands=['start', 'on', 'off'])
+def handle_commands(message):
+    # Handle /start, /on, and /off commands
     send_log(bot, message)
     global state
-    state = True
-    # Handle the /on command
-    bot.reply_to(message, "BOT ON")
+    if message.text == '/start':
+        bot.reply_to(message, 'Welcome to the Spotify Downloader Bot!\n\nSend me a song name or a Spotify link to download.')
+    elif message.text == '/on':
+        state = True
+        bot.reply_to(message, "BOT ON")
+    elif message.text == '/off':
+        state = False
+        bot.reply_to(message, "BOT OFF")
 
-@bot.message_handler(commands=['off'])
-def handle_off(message):
-    global state
-    state = False
-    # Handle the /off command
-    bot.reply_to(message, "BOT OFF")
-
-# Handler for receiving messages
 @bot.message_handler(func=lambda message: True)
 def download_song(message):
+    # Handle song download requests
     send_log(bot, message)
-    if not state and message.chat.id != admin_user :
+    if not state and message.chat.id != admin_user:
         return
 
     global last_message_id
-
-    # Check if this is the same message as the previous one
     if last_message_id == message.message_id:
         return
-
-    # Store the current message ID as the most recent one
     last_message_id = message.message_id
 
-    # Extract the input from the message
     input_text = message.text.strip()
 
-    # Create the "./spotify" directory if it doesn't exist
-    if not os.path.exists('./spotify'):
-        os.makedirs('./spotify')
+    os.makedirs('./spotify', exist_ok=True)
     os.chdir('./spotify')
-
-    # Check if the input is a Spotify link or a song name
-    if input_text.startswith('http') and 'spotify.com' in input_text:
-        # Input is a Spotify link
-        spotify_link = input_text
-        if 'album' in input_text:
-            response = 'Downloading Album ...'
-        elif 'playlist' in input_text:
-            response = 'Downloading Playlist ...'
-        else :
-            response = 'Downloading Track ...'
-            
-    else:
-        # Input is a song name
-        spotify_link = f'\'{input_text}\''
-        response = 'Downloading Track ...'
-
-    # Run SpotDL command in the shell to download the song
+    
+    spotify_link = input_text if input_text.startswith('http') and 'spotify.com' in input_text else f"'{input_text}'"
+    response = 'Downloading Album ...' if 'album' in input_text else 'Downloading Playlist ...' if 'playlist' in input_text else 'Downloading Track ...'
     command = f'spotdl --threads 6 {spotify_link}'
-    
-    start_time = time.time()  # Record the start time
-    
-    
-    
+
+    start_time = time.time()
     wait = bot.reply_to(message, response)
     result = os.system(command)
-    
-    end_time = time.time()  # Record the end time
-    download_time = end_time - start_time  # Calculate the download time
+    end_time = time.time()
+    download_time = end_time - start_time
     bot.delete_message(message.chat.id, wait.message_id)
 
     if result != 0:
         bot.reply_to(message, "There was some error in the Download.")
 
-    # Get the list of downloaded song files
     song_files = [f for f in os.listdir('.') if os.path.isfile(f) and os.path.getsize(f) > 10000]
-
-    # Send each song file back to the user
     for song_file in song_files:
-        # Send the song file as an audio to the user
         bot.send_audio(message.chat.id, audio=open(song_file, 'rb'))
-        
-    # Send the download time to the user
-    bot.send_message(message.chat.id, f"Download time: {(int(download_time))//60} minutes {(int(download_time))%60} seconds")
 
-    # Change back to the previous directory
+    bot.send_message(message.chat.id, f"Download time: {(int(download_time)) // 60} minutes {(int(download_time)) % 60} seconds")
+
     os.chdir('..')
-
-    # Delete the entire "./spotify" directory
     os.system('rm -rf ./spotify')
