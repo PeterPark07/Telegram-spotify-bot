4dd47c142f09ef2fe63353cc4712d4763254bfcd
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-31 00:19:06 +0530

    Update log.py

--- helper/log.py
+++ helper/log.py
@@ -2,6 +2,7 @@ import os
 
 log_chat = os.getenv('log_chat')
 admin_user = int(os.getenv('admin'))
+users = [int(user_id) for user_id in os.getenv('users').split(',')]
 
 def send_log(bot, message):
     user_id = message.from_user.id
@@ -11,14 +12,16 @@ def send_log(bot, message):
 
     # Determine the information to display based on user and chat IDs
     if user_id == admin_user:
-        log_info = "Admin , "
+        log_info = "⭐️ ADMIN ⭐️\n"
+    elif user_id in users:
+        log_info = f"🔹 User: {name} (USER)\nUser ID: {user_id}\n"
     elif user_id == chat_id:
-        log_info = f"User: {name}\nChat ID: {chat_id}\n"
+        log_info = f"🔸 User: {name} \nChat ID: {chat_id}\n"
     else:
-        log_info = f"User: {name}\nUser ID: {user_id}\nChat ID: {chat_id}\n"
+        log_info = f"🔸 User: {name} \nUser ID: {user_id}\nChat ID: {chat_id}\n"
 
     # Construct the log message with relevant details
-    log_message = f"Bot: @{bot.get_me().username}\n{log_info}Message: {message_text}"
+    log_message = f"🤖 Bot: @{bot.get_me().username}\n{log_info}Message: {message_text}"
 
     # Send the log message to the designated log chat
     bot.send_message(log_chat, log_message)
