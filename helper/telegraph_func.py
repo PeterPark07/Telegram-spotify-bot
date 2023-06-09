426fbfff499c7ea5faf9ae36a0d785c4ceac1ae3
PeterPark07 <slimshadx69@gmail.com>
2023-06-10 01:24:39 +0530

    Update telegraph_func.py

--- helper/telegraph_func.py
+++ helper/telegraph_func.py
@@ -3,20 +3,13 @@ import os
 
 telegraph = Telegraph(os.getenv('telegraph'))
 path = os.getenv('telegraph_path')
-
-def get_page(return_content=True, return_html=True):
-    page = telegraph.get_page(path=path,return_content=return_content,return_html=return_html)
-    return page['title'], page['content']
-    
-def edit_page(title, html_content):
-    page = telegraph.edit_page(path=path,title=title,html_content=html_content,author_name='bots')
-    return
     
 def logg(new_content):
     try:
-        title, content = get_page()
-        content = content + new_content
-        edit_page(title, content)
+        page = telegraph.get_page(path=path,return_content=True,return_html=True)
+        title, content = page['title'], page['content']
+        content = new_content + content
+        telegraph.edit_page(path=path,title=title,html_content=content,author_name='bots')
         return True
     except:
         return False
